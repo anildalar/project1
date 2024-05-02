312b28537410d850ea5ced65de91e01af9b2458c
anildalar <dollorinfotech@gmail.com>
2024-05-02 10:56:13 +0530

    my new project1

---
+++ info.php
@@ -0,0 +1,17 @@
+<?php // PHP Start Tag
+
+    echo 'Hello<br>';
+    echo("My name is anil<br>");
+    print 'all Code is executing at server side<br>';
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+    </head>
+    <body>
+        <h1>This is a Heading</h1>
+        <p>This is a paragraph.</p>
+    </body>
+</html>
